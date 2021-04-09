15f0384b49828de019a96f2740be7a416ca32d39
isyel <isykaal4saviour@gmail.com>
2021-04-09 15:32:09 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const { ApolloServer, PubSub } = require("apollo-server");
+const mongoose = require("mongoose");
+
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
+const pubSub = new PubSub();
+
+const port = process.env.port || 5000;
+
+const { MONGODB } = require("./config.js");
+const server = new ApolloServer({
+	typeDefs,
+	resolvers,
+	context: ({ req }) => ({ req, pubSub }),
+});
+
+mongoose
+	.connect(MONGODB, { useNewUrlParser: true })
+	.then(() => {
+		console.log("MongoDB Connected");
+		return server.listen({ port: port });
+	})
+	.then((res) => {
+		console.log(`Server is Running at ${res.url}`);
+	})
+	.catch((error) => console.error(error));
